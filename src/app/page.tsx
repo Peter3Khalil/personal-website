6d8fd8462cbe8a718f2ea8fe0aa0688b1a0270e8
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-10-25 01:24:40 +0300

    ✏️ Adding social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,26 @@ import Lottie, { Options } from 'react-lottie';
 import animationData from '@/animation3.json';
 import { useEffect, useRef, useState } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
+import { GithubIcon, GmailIcon, LinkedinIcon } from '@/components/shared/icons';
+
+const SOCIAL_LINKS = [
+  {
+    title: 'Github account',
+    icon: GithubIcon,
+    href: 'https://github.com/Peter3Khalil',
+  },
+  {
+    title: 'Linkedin account',
+    icon: LinkedinIcon,
+    href: 'https://www.linkedin.com/in/peter-khalil-frontend-developer',
+  },
+  {
+    title: 'Gmail account',
+    icon: GmailIcon,
+    href: 'mailto:peter.khalil.fahmy@gmail.com',
+  },
+];
+
 const Home = () => {
   const [isSvgLoaded, setIsSvgLoaded] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
@@ -26,7 +46,7 @@ const Home = () => {
     <div>
       <Header />
       <main className="container flex flex-col-reverse items-center justify-between gap-4 py-8 lg:flex-row">
-        <div className="w-full">
+        <section className="flex w-full flex-col md:items-center lg:items-start">
           <div className="flex flex-col md:items-center lg:items-start">
             <span className="text-3xl font-bold lg:text-5xl">
               Hi, I&apos;m Peter.
@@ -49,15 +69,32 @@ const Home = () => {
               <b>React</b>, <b>Next.js</b>, and <b>Tailwind CSS</b>.
             </p>
           </div>
-        </div>
-        <div className="relative h-[300px] w-full md:h-[50vh] md:max-w-[800px] lg:h-[90vh]">
+          <ul className="mt-12 flex flex-wrap items-center gap-4">
+            {SOCIAL_LINKS.map(({ href, title, icon: Icon }, index) => (
+              <li
+                key={index}
+                className="group size-8 rounded border-2 border-foreground duration-200 hover:bg-foreground"
+              >
+                <a
+                  href={href}
+                  target="_blank"
+                  title={title}
+                  className="block size-full p-1"
+                >
+                  <Icon className="group-hover:fill-background" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section className="relative h-[300px] w-full md:h-[50vh] md:max-w-[800px] lg:h-[90vh]">
           <div ref={ref} className="size-full">
             <Lottie options={defaultOptions} />
           </div>
           {!isSvgLoaded && (
             <Skeleton className="absolute left-0 top-0 z-10 size-full" />
           )}
-        </div>
+        </section>
       </main>
     </div>
   );
