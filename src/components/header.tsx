c30ecbedd06f09529a152a04099ef9ea22acd5fd
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-10-25 00:51:35 +0300

    💄 Changing logo svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,14 +11,46 @@ const Header = () => {
     <header className="container flex items-center justify-between border-b border-muted bg-background py-4 text-foreground md:py-6">
       <a href="#" className="flex items-center gap-3">
         <svg
-          width="40"
-          height="40"
-          viewBox="0 0 40 40"
           xmlns="http://www.w3.org/2000/svg"
-          className="fill-current"
+          viewBox="0 0 24 24"
+          id="Computer"
+          className="size-6 md:size-10"
         >
-          <path d="M31.5294 14.2118C31.5294 18.8819 28.7623 22.9082 24.7793 24.7351C26.4612 23.3496 27.5322 21.248 27.5322 18.896C27.5322 14.7642 24.2259 11.4042 20.112 11.3205C20.0574 11.3167 20.0075 11.3167 19.9529 11.3167C19.8983 11.3167 19.8485 11.3167 19.7939 11.3205C18.2673 11.4033 17.0588 12.6663 17.0588 14.2108V28.6814C17.0588 33.4748 13.1699 37.3638 8.37646 37.3638V14.2118C8.37646 7.81928 13.5605 2.63528 19.9529 2.63528C26.3454 2.63528 31.5294 7.81928 31.5294 14.2118Z" />
-          <path d="M21.9992 16.2582C23.1294 15.128 23.1294 13.2956 21.9992 12.1654C20.8689 11.0351 19.0365 11.0351 17.9063 12.1654C16.7761 13.2956 16.7761 15.128 17.9063 16.2582C19.0365 17.3884 20.8689 17.3884 21.9992 16.2582Z" />
+          <rect
+            width="23"
+            height="17"
+            x=".5"
+            y="1.5"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            rx="1.5"
+            ry="1.5"
+            className="colorStroke303c42 svgStroke stroke-primary"
+          ></rect>
+          <path
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.5 18.5s.21 4-2 4h9c-2.21 0-2-4-2-4M.5 14.5h23"
+            className="colorStroke303c42 svgStroke stroke-primary"
+          ></path>
+          <circle
+            cx="12"
+            cy="16.5"
+            r=".5"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="colorStroke303c42 svgStroke stroke-primary"
+          ></circle>
+          <path
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.5 5.5 6.5 8l3 2.5M14.5 5.5l3 2.5-3 2.5M10.5 11.5l3-7"
+            className="colorStroke303c42 svgStroke stroke-primary"
+          ></path>
         </svg>
         <span className="text-xl font-bold">Peter</span>
       </a>
