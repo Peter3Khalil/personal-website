199817c96d91270e5c099bf43a1a781c06207665
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-12-02 03:22:16 +0200

    💄 Updating About Me section with personalized content

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -15,19 +15,29 @@ const AboutMe: FC<React.HTMLAttributes<HTMLDivElement>> = ({
       {...props}
     >
       <AboutMeSvg />
-      <article className="flex-1">
+      <article className="flex-1 md:max-w-[700px]">
         <h2 className="section-title">About Me</h2>
         <div className="mt-8 flex flex-col gap-4 text-muted-foreground md:gap-6 md:text-lg">
-          {Array.from({ length: 3 }).map((_, i) => (
-            <p key={i}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
-              omnis nam! Vel voluptatum recusandae commodi repellendus rerum
-              saepe! Dolorum illo blanditiis natus ducimus neque modi possimus
-              dolor repudiandae error nam. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Ratione, omnis nam! Vel voluptatum
-              recusandae commodi repellendus rerum
-            </p>
-          ))}
+          <p>
+            I’m a passionate <b>frontend developer</b> with a love for creating
+            seamless, responsive, and user-friendly designs. Crafting intuitive
+            user experiences and breathing life into pixels through code is what
+            excites me every day.
+          </p>
+          <p>
+            I specialize in <b>web development</b>, focusing on{' '}
+            <b>responsive design</b> and ensuring every interface is not only
+            functional but delightful to use. My work revolves around blending
+            creativity with functionality to deliver clean, modern, and
+            impactful web solutions.
+          </p>
+          <p>
+            I’m always eager to learn new technologies and frameworks to enhance
+            my skills and stay up-to-date with the latest trends in the frontend
+            development world. I’m currently exploring the world of <b>React</b>{' '}
+            and <b>Next.js</b> to build fast, dynamic, and{' '}
+            <b>high-performing</b> web applications.
+          </p>
         </div>
       </article>
     </section>
