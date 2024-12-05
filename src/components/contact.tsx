a9064f74a2ff2e3bd3ba542870164970afceb6c8
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-12-05 03:27:39 +0200

    ✨ Handling sending email using emailjs

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,16 +3,20 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { SOCIAL_LINKS } from '@/constants/data';
 import { cn } from '@/lib/utils';
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 const INPUTS: React.HTMLProps<HTMLInputElement>[] = [
   {
     placeholder: 'Your Name',
     required: true,
     minLength: 3,
+    name: 'name',
   },
   {
     placeholder: 'Your Email',
     type: 'email',
+    name: 'email',
     required: true,
   },
 ];
@@ -20,10 +24,38 @@ const Contact: FC<React.HTMLAttributes<HTMLDivElement>> = ({
   className,
   ...props
 }) => {
+  const form = useRef<HTMLFormElement | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        form.current!,
+        {
+          publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+        },
+      )
+      .then(
+        () => {
+          form.current?.reset();
+          alert('SUCCESS!');
+        },
+        (error) => {
+          alert(`FAILED...: ${error.text}`);
+        },
+      )
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
   return (
     <div className={cn('container dark:bg-accent/50', className)} {...props}>
       <div className="mx-auto flex max-w-[1400px] flex-col justify-between gap-10 *:flex-1 md:flex-row md:items-center lg:gap-x-24">
-        <form className="space-y-4">
+        <form ref={form} onSubmit={sendEmail} className="space-y-4">
           {INPUTS.map((input, index) => (
             <Input
               className="bg-transparent dark:border-muted-foreground/30"
@@ -36,9 +68,12 @@ const Contact: FC<React.HTMLAttributes<HTMLDivElement>> = ({
             required
             className="max-h-52 min-h-40 bg-transparent dark:border-muted-foreground/30"
             minLength={10}
+            name="message"
           />
           <div className="flex flex-col justify-between gap-4 sm:flex-row sm:items-center">
-            <Button className="md:flex-1">Get in Touch</Button>
+            <Button disabled={isLoading} className="md:flex-1">
+              {isLoading ? 'Sending...' : 'Get in Touch'}
+            </Button>
             <ul className="flex flex-wrap items-center gap-4">
               {SOCIAL_LINKS.map(({ href, title, icon: Icon }, index) => (
                 <li
